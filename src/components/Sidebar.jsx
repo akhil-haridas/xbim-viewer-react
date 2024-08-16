e7c222191d589022b1dd01b70161a9267088d205
akhil-dignizant <akhil.k@dignizant.com>
2024-08-16 18:15:35 +0530

    sidebar component with tools

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import { Viewer } from "@xbim/viewer";
 
 const Sidebar = () => {
-  return <div className="SideBarWrapper">SideBar</div>;
+
+  useEffect(() => {
+    const viewer2 = new Viewer("viewer2");
+    var sync = () => {
+      viewer2.mvMatrix = viewer2.mvMatrix;
+      window.requestAnimationFrame(sync);
+    };
+    window.requestAnimationFrame(sync);
+    viewer2.loadAsync('../assets/sample.wexbim');
+    viewer2.on('loaded', () => {
+      viewer2.start();
+    });
+  }, [])
+
+  return (
+    <div className="SideBarWrapper">
+      <div className="ContentWrapper"></div>
+      <div className="SmallView">
+        <div
+          style={{ position: "absolute", left: 0, bottom: 0, width: 400, height: 200 }}
+        >
+          <canvas id="viewer2" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
