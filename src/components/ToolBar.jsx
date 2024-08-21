79aa9b2c0fdbec32695189567f314f729d387c8f
akhil-haridas <4khilharidas@gmail.com>
2024-08-21 10:47:01 +0530

    code format by prettier

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -5,38 +5,80 @@ import React from "react";
 const ToolBar = () => {
   return (
     <div className="ToolBarWrapper">
-      <button onClick={() => {
-        window.viewer.navigationMode = 'orbit'
-      }}>Orbit</button>
-      <button onClick={() => {
-        window.viewer.navigationMode = 'free-orbit'
-      }}>Free Orbit</button>
-      <button onClick={() => {
-        window.viewer.navigationMode = 'pan'
-      }}>Pan</button>
-      <button onClick={() => {
-        window.viewer.navigationMode = 'zoom'
-      }}>Zoom</button>
-      <button onClick={() => {
-        window.viewer.navigationMode = 'look-around'
-      }}>Look Around</button>
-      <button onClick={() => {
-        window.viewer.navigationMode = 'walk'
-      }}>Walk</button>
-      <button onClick={() => {
-        window.viewer.show(ViewType.DEFAULT)
-      }}>Deafult</button>
-      <button onClick={() => {
-        const elements = window.viewer.getProductsWithState(State.HIGHLIGHTED);
-        window.viewer.zoomTo(elements);
-      }}>Zoom to Selection</button>
-      <button onClick={() => {
-        window.plane.stopped = false;
-      }}>Clip Model</button>
-      <button onClick={() => {
-        window.viewer.unclip();
-        window.plane.stopped = true;
-      }}>Reset Clipping</button>
+      <button
+        onClick={() => {
+          window.viewer.navigationMode = "orbit";
+        }}
+      >
+        Orbit
+      </button>
+      <button
+        onClick={() => {
+          window.viewer.navigationMode = "free-orbit";
+        }}
+      >
+        Free Orbit
+      </button>
+      <button
+        onClick={() => {
+          window.viewer.navigationMode = "pan";
+        }}
+      >
+        Pan
+      </button>
+      <button
+        onClick={() => {
+          window.viewer.navigationMode = "zoom";
+        }}
+      >
+        Zoom
+      </button>
+      <button
+        onClick={() => {
+          window.viewer.navigationMode = "look-around";
+        }}
+      >
+        Look Around
+      </button>
+      <button
+        onClick={() => {
+          window.viewer.navigationMode = "walk";
+        }}
+      >
+        Walk
+      </button>
+      <button
+        onClick={() => {
+          window.viewer.show(ViewType.DEFAULT);
+        }}
+      >
+        Deafult
+      </button>
+      <button
+        onClick={() => {
+          const elements = window.viewer.getProductsWithState(
+            State.HIGHLIGHTED
+          );
+          window.viewer.zoomTo(elements);
+        }}
+      >
+        Zoom to Selection
+      </button>
+      <button
+        onClick={() => {
+          window.plane.stopped = false;
+        }}
+      >
+        Clip Model
+      </button>
+      <button
+        onClick={() => {
+          window.viewer.unclip();
+          window.plane.stopped = true;
+        }}
+      >
+        Reset Clipping
+      </button>
     </div>
   );
 };
